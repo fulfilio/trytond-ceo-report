714bf0d53ae682a321113d660a3f3de6b3526e35
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2016-08-21 23:39:55 +0530

    Maintained by fulfil.io

--- ceo_report.py
+++ ceo_report.py
@@ -2,8 +2,6 @@
 """
     ceo_report.py
 
-    :copyright: (c) 2014 by Openlabs Technologies & Consulting (P) Limited
-    :license: BSD, see LICENSE for more details.
 """
 import json
 from datetime import datetime, time, date
